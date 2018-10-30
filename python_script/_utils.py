ef547bd87a86e7251219bf102e1c5d8795f74ca0
askr-ssy <ask.shen@1data.info>
2018-10-30 20:38:45 +0800

    Update python_script/_utils.py

--- python_script/_utils.py
+++ python_script/_utils.py
@@ -89,3 +89,91 @@ def html_filter(input_text):
     """
     return re.sub('<\/?[\s\S]*?(?:".*")*>','',input_text)
 
+def filter_tags(htmlstr):
+    """
+    Python通过正则表达式去除(过滤)HTML标签
+
+    --
+    :param htmlstr:
+    :return:
+    """
+    # CDATA
+    re_cdata = re.compile('//<!\
+    CDATA\[[ >]∗ //\
+    CDATA\[[ >]∗ //\
+    \] > ',re.I)
+    
+    re_script = re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>', re.I)
+    # Script
+    
+    re_style = re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', re.I)
+    # style
+    
+    re_br = re.compile('<br\s*?/?>')
+    # 换行
+    
+    re_h = re.compile('</?\w+[^>]*>')
+    # HTML标签
+    
+    re_comment = re.compile('<!--[^>]*-->')
+    # HTML注释
+    
+    s = re_cdata.sub('', htmlstr)
+    # 去掉CDATA
+    
+    s = re_script.sub('', s)  # 去掉SCRIPT
+    
+    s = re_style.sub('', s)
+    # 去掉style
+    
+    s = re_br.sub('\n', s)
+    # 将br转换为换行
+    
+    s = re_h.sub('', s)  # 去掉HTML 标签
+    
+    s = re_comment.sub('', s)
+    # 去掉HTML注释
+    
+    # 去掉多余的空行
+    blank_line = re.compile('\n+')
+    s = blank_line.sub('\n', s)
+    s = replaceCharEntity(s)  # 替换实体
+    return s
+
+
+
+def replaceCharEntity(htmlstr):
+    """
+    :param htmlstr:HTML字符串
+    :function:过滤HTML中的标签
+    """
+    CHAR_ENTITIES = {'nbsp': ' ', '160': ' ',
+                     'lt': '<', '60': '<',
+                     'gt': '>', '62': '>',
+                     'amp': '&', '38': '&',
+                     'quot': '"', '34': '"', }
+
+    re_charEntity = re.compile(r'&#?(?P<name>\w+);')
+    sz = re_charEntity.search(htmlstr)
+    while sz:
+        entity = sz.group()  # entity全称，如>
+        key = sz.group('name')  # 去除&;后entity,如>为gt
+        try:
+            htmlstr = re_charEntity.sub(CHAR_ENTITIES[key], htmlstr, 1)
+            sz = re_charEntity.search(htmlstr)
+        except KeyError:
+            # 以空串代替
+            htmlstr = re_charEntity.sub('', htmlstr, 1)
+            sz = re_charEntity.search(htmlstr)
+    return htmlstr
+
+
+def repalce(s, re_exp, repl_string):
+    return re_exp.sub(repl_string,s)
+
+def cleaning_data(strs):
+    strs=str(strs).replace('<p>&nbsp; &nbsp; &nbsp; &nbsp;','').replace('</p><p><br></p>','').replace('<br>','').replace('</p>','').replace('<p>','').replace('       ','').replace('[图片]','').strip()
+    strs=filter_tags(strs)
+    return replaceCharEntity(strs)
+
+
