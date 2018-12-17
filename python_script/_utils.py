17d0709deaa592226af029fc2ad6e8c821522366
askr-ssy <ask.shen@1data.info>
2018-12-17 15:43:46 +0800

    Update _utils.py

--- python_script/_utils.py
+++ python_script/_utils.py
@@ -169,3 +169,28 @@ def word_count(*args,**kwargs):
             content_count[word]+=1
     content_count=sorted(content_count.items(),key=lambda x:x[1],reverse=True)
     return content_count
+
+def _replaceCharEntity(htmlstr):
+    """
+    :param htmlstr:HTML字符串
+    :function:过滤HTML中的标签
+    """
+    CHAR_ENTITIES = {'nbsp': ' ', '160': ' ',
+                     'lt': '<', '60': '<',
+                     'gt': '>', '62': '>',
+                     'amp': '&', '38': '&',
+                     'quot': '"', '34': '"', }
+
+    re_charEntity = re.compile(r'&#?(?P<name>\w+);')
+    sz = re_charEntity.search(htmlstr)
+    while sz:
+        entity = sz.group()  # entity全称，如>
+        key = sz.group('name')  # 去除&;后entity,如>为gt
+        try:
+            htmlstr = re_charEntity.sub(CHAR_ENTITIES[key], htmlstr, 1)
+            sz = re_charEntity.search(htmlstr)
+        except KeyError:
+            # 以空串代替
+            htmlstr = re_charEntity.sub('', htmlstr, 1)
+            sz = re_charEntity.search(htmlstr)
+    return htmlstr
