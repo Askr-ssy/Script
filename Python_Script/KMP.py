b16e9db722615011f83e829f86644eb49d1d9179
askr-ssy <askr.shen@1data.info>
2019-03-18 15:54:23 +0800

    Create KMP.py

---
+++ Python_Script/KMP.py
@@ -0,0 +1,39 @@
+S='BBC ABCDAB ABCDABCDABDE'
+
+P='ABCDABD'
+
+def _index(S,P):
+    def getNext(P):
+        pLen=len(P)
+        _next=[None]*pLen
+        _next[0]=-1
+        k=-1
+        j=0 
+        while j<pLen-1:
+            if k==-1 or P[j]==P[k]:
+                k+=1
+                j+=1
+                _next[j]=k
+            else:
+                k=_next[k]
+        return _next
+    
+    _next=getNext(P)
+    sLen=len(S)
+    pLen=len(P)
+
+    i=0             # 主字符串的标志位
+    j=0             # 模式字符串的标志位
+
+    while i<sLen and j <pLen:
+        if j==-1 or S[i]==P[j]:
+            i+=1
+            j+=1
+        else:
+            j=_next[j]
+    if j==pLen:
+        return i-j
+    else:
+        return -1
+print(S.index(P))
+print(_index(S,P))
