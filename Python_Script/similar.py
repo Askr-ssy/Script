344d6bf19d0f555ff42f1b71f1f4b94468fee02f
askr-ssy <askr.shen@1data.info>
2019-04-02 15:13:48 +0800

    add

--- Python_Script/similar.py
+++ Python_Script/similar.py
@@ -3,18 +3,6 @@ def LCS(a,b):
         pass
     pass
 
-def Jaccardsimilarity(a,b):
-    '''
-    jaccard 系数
-    '''
-    if not a or not b:
-        raise Exception('value error')
-    if not isinstance(a,set):
-        a=set(a)
-    if not isinstance(b,set):
-        b=set(b)
-    return len((a.intersection(b)))/(len(b) if len(a)>len(b) else len(a))
-
 def Edit_Distance(a,b):
     '''
     编辑距离
