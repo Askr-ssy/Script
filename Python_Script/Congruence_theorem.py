aaaf302ebaf0ef0f950f4b2f475c8ae8d095e195
askr-ssy <askr.shen@1data.info>
2019-04-03 16:30:07 +0800

    次方求模

--- Python_Script/Congruence_theorem.py
+++ Python_Script/Congruence_theorem.py
@@ -5,5 +5,22 @@ def congruence(*args,**kwargs):
     m=args[2]
     return m%(a-b)==0
 
+def powmod(*args,**kwargs):
+    '''
+    n^b mod c
+    '''
+    a=args[0]
+    b=args[1]   
+    c=args[2]
+    if b==0:
+        return 1
+    if b==1:
+        return a%c
+    temp=powmod(a,b>>1,c)
+    temp=temp*temp%c
+    if b %2:
+           temp=temp*a%c%c
+    return temp
 if __name__ == "__main__":
     print(congruence(13,17,2))
+    print(powmod(11,12345,12345))
