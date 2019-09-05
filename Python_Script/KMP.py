ba57917381bb6efe17af53ba8fd591986cef9083
askr-ssy <ask.shen@1data.info>
2019-09-05 12:33:05 +0800

    add

--- Python_Script/KMP.py
+++ Python_Script/KMP.py
@@ -6,16 +6,16 @@ def _index(S,P):
     def getNext(P):
         pLen=len(P)
         _next=[None]*pLen
-        _next[0]=-1
-        k=-1
-        j=0 
-        while j<pLen-1:
-            if k==-1 or P[j]==P[k]:
+        _next[0]=-1                     # 1 断言：在J=0的时候，值为-1
+        k=-1        # K为当前失败后，下一步要跳到的位置
+        j=0         # J为next数组中的索引
+        while j<pLen-1:                 
+            if k==-1 or P[j]==P[k]:     # 2.1.1 当P[J]==P[K] 即,前缀与后缀相同时
                 k+=1
                 j+=1
-                _next[j]=k
-            else:
-                k=_next[k]
+                _next[j]=k              # 2.1.2 P[J+1] 与 P[K+1] 成立
+            else:                       # 2.2.1 当P[J] != P[k] 时,即 前缀与后缀不同
+                k=_next[k]              # 2.2.2 最长前缀后缀匹配失败时,K通过next数组跳到失败的位置 成立
         return _next
     
     _next=getNext(P)
