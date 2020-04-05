be9a76d544f6346af0e651ece44872a5b5f83159
askr-ssy <Askr-ssy@users.noreply.github.com>
2020-04-06 00:18:13 +0800

    Update ubuntu_initialization.sh

--- Shell/ubuntu_initialization.sh
+++ Shell/ubuntu_initialization.sh
@@ -4,10 +4,14 @@
 
 release_name=$(lsb_release -cs)
 
+if ! [ -x $(dpkg -l | grep neofetch) ] ;
+then echo "ping"
+fi
+
 # --- end
 
 # --- 读取命令行参数
-export https_proxy=socks5://127.0.0.1:1080
+# export https_proxy=socks5://127.0.0.1:1080
 
 # --- end
 
@@ -16,7 +20,7 @@ export https_proxy=socks5://127.0.0.1:1080
 # --- end
 
 # --- 安装 vim 等系统软件
-apt install vim
+# apt install vim
 # --- end
 
 # --- 安装 用户软件
