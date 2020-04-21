e578d28450410bf7e059fee61d377e0c4a99d75f
askr <ask.shen@1data.info>
2020-04-21 11:26:14 +0800

    update unicode header

--- Cpp/lib/askr/Unicode.hpp
+++ Cpp/lib/askr/Unicode.hpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<vector>
 #include<ostream>
+#include<cassert>
 namespace askr{
 
 
@@ -22,10 +23,11 @@ struct UnicodeStr{
     uint32_t offset;    // offset on array (bytes)
     size_t length;         // bytes count
     uint32_t unicode_offset;    // offset on array(unicode count)
+    size_t unicode_length;
 
     UnicodeStr():unicode(unicode),offset(0),length(0),unicode_offset(0){}
     UnicodeStr(uint32_t unicode,uint32_t o,size_t l):unicode(unicode),offset(o),length(l){}
-    UnicodeStr(uint32_t unicode,uint32_t o,size_t l,uint32_t unicode_offset):unicode(unicode),offset(o),length(l),unicode_offset(unicode_offset){}
+    UnicodeStr(uint32_t unicode,uint32_t o,size_t l,uint32_t unicode_offset,size_t unicode_length):unicode(unicode),offset(o),length(l),unicode_offset(unicode_offset),unicode_length(unicode_length){}
 };
 typedef std::vector<uint32_t> Unicode;
 typedef std::vector<UnicodeStr> UnicodeStrArray;
@@ -113,13 +115,91 @@ inline UnicodeStrLite DecodeUnicodeInString(const char *str,size_t len){
     return us;
 }
 
+inline bool DecodeUnicodesInString(const char *s,size_t len,UnicodeStrArray &us){
+    us.clear();
+    us.reserve(len/2);
+    for (uint32_t i=0,j=0;i<len;){
+        UnicodeStrLite ul = DecodeUnicodeInString(s+i,len-i);
+        if (ul.length==0){
+            us.clear();
+            return false;
+        }
+        UnicodeStr x(ul.unicode,i,ul.length,j,1);
+        us.push_back(x);
+        i+=ul.length;
+        ++j;
+    }
+    return true;
+}
+
+inline bool DecodeUnicodesInString(const char *s,size_t len,Unicode &unicode){
+    unicode.clear();
+    UnicodeStrArray us;
+    if (!DecodeUnicodesInString(s,len,us)){
+        return false;
+    }
+    unicode.reserve(us.size());
+    for (size_t i=0;i<us.size();++i){
+        unicode.push_back(us[i].unicode);
+    }
+    return true;
+}
+
+inline bool DecodeUnicodesInString(const std::string &s,UnicodeStrArray &us){
+    return DecodeUnicodesInString(s.c_str(),s.size(),us);
+}
+
+inline bool DecodeUnicodesInString(const std::string &s,Unicode &unicode){
+    return DecodeUnicodesInString(s.c_str(),s.size(),unicode);
+}
+
+inline Unicode DecodeUnicodesInString(const std::string &s){
+    Unicode result;
+    DecodeUnicodesInString(s,result);
+    return result;
+}
+
+inline Word GetWordFromUnicodes(const std::string &s,UnicodeStrArray::const_iterator l,UnicodeStrArray::const_iterator r){
+    assert(r->offset>=l->offset);
+    uint32_t len = r->offset+r->length - l->offset;
+    uint32_t unicode_length = r->unicode_offset + r->unicode_length - l->unicode_offset;
+    return Word(s.substr(l->offset,len),l->offset,l->unicode_offset,unicode_length);
+}
+inline std::string GetStringFromUnicodes(const std::string &s,UnicodeStrArray::const_iterator l,UnicodeStrArray::const_iterator r){
+    assert(r->offset>=l->offset);
+    uint32_t len = r->offset+r->length-l->offset;
+    return s.substr(l->offset,len);
+}
+inline void GetWordsFromWordRanges(const std::string &s,const std::vector<WordRange> &wrs,std::vector<Word> &words){
+    for (size_t i=0;i<wrs.size();++i){
+        words.push_back(GetWordFromUnicodes(s,wrs[i].left,wrs[i].right));
+    }
+}
+inline std::vector<Word> GetWordsFromWordRanges(const std::string &s,std::vector<WordRange> &wrs){
+    std::vector<Word> result;
+    GetWordsFromWordRanges(s,wrs,result);
+    return result;
+}
+inline void GetStringsFromWords(const std::vector<Word> &words,std::vector<std::string> &strs){
+    strs.resize(words.size());
+    for (size_t i=0;i<words.size();++i){
+        strs[i]=words[i].word;
+    }
+}
+//单个unicode 字符
+inline bool IsSingleWord(const std::string &s){
+    UnicodeStrLite ul = DecodeUnicodeInString(s.c_str(),s.size());
+    return ul.length == s.size();
+}
+
+
 inline std::ostream &operator<<(std::ostream &os,const Word &w){
     return os << "{\"word\": \"" << w.word << "\", \"offset\": " << w.offset << "}" << std::endl;
 }
 
-inline std::ostream &operator<<(std::ostream &os,const Unicode &r)
+inline std::ostream &operator<<(std::ostream &os,const UnicodeStr &r)
 {
-    return os << "{\"rune\": \"" << r.unicode << "\", \"offset\": " << r.offset << ", \"len\": " << r.length << "}";
+    return os << "{\"unicode\": \"" << r.unicode << "\", \"offset\": " << r.offset << ", \"len\": " << r.length << "}";
 }
 }
 #endif
