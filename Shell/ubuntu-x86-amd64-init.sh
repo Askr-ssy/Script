dd5656e8ce63c3f39f77f5310a39cf1924e45272
askr-ssy <Askr-ssy@users.noreply.github.com>
2020-05-28 21:36:55 +0800

    Update ubuntu-x86-amd64-init.sh

--- Shell/ubuntu-x86-amd64-init.sh
+++ Shell/ubuntu-x86-amd64-init.sh
@@ -6,7 +6,6 @@ release_name=$(lsb_release -cs)
 # TODO 设置国外源或者国内源 修改文件
 # --- end
 
-
 # --- 读取命令行参数
 # TODO 是否代理
 # --- end
@@ -32,6 +31,8 @@ done
 wget -c https://go.microsoft.com/fwlink/?LinkID=760868 -O ./$deb_tempdir/code.deb
 wget -c https://d1.music.126.net/dmusic/netease-cloud-music_1.2.1_amd64_ubuntu_20190428.deb -O ./$deb_tempdir/netease.deb
 # TODO 循环安装deb_tempdir 目录下的deb文件
+dpkg -i ./$deb_tempdir/code.deb
+dpkg -i ./$deb_tempdir/netease.deb
 # --- end
 
 # 需要单独安装的软件
@@ -39,13 +40,10 @@ install_docker
 install_qbittorrent
 install_postman
 install_telegram
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 # --- end
-
-# TODO 安装 calibre vpn
 
 # --- 转移文件 恢复数据 处理垃圾数据
 # TODO 恢复国内源或者国外源
-
 sudo rm -rf ./$deb_tempdir
-
 # --- end
